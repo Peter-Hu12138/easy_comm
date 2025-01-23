cc7c61bdbea19957c00d332afaf567d5d37c445b
Peter-Hu12138 <hjtwillbeok@gmail.com>
2025-01-23 02:15:29 -0500

    feat!: clients can now create multiple rooms. achieved by wrapping client chat_window and socket into a wrapper class ChatWindow and a manager class

--- chat_flex.py
+++ chat_flex.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import selectors
 import socket
 import sys
@@ -5,98 +6,202 @@ import types
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
 from help_manual import cmd
+from tkinter import ttk
+
+STX = b'\x02'  # Start of Text
+ETX = b'\x03'  # End of Text
+
 
 class ChatWindow(tk.Toplevel):
-    def __init__(self, title, master = None, cnf = ..., *, background = ..., bd = 0, bg = ..., border = 0, borderwidth = 0, class_ = "Toplevel", colormap = "", container = False, cursor = "", height = 0, highlightbackground = ..., highlightcolor = ..., highlightthickness = 0, menu = ..., name = ..., padx = 0, pady = 0, relief = "flat", screen = "", takefocus = 0, use = ..., visual = "", width = 0):
-        super().__init__(master, cnf, background=background, bd=bd, bg=bg, border=border, borderwidth=borderwidth, class_=class_, colormap=colormap, container=container, cursor=cursor, height=height, highlightbackground=highlightbackground, highlightcolor=highlightcolor, highlightthickness=highlightthickness, menu=menu, name=name, padx=padx, pady=pady, relief=relief, screen=screen, takefocus=takefocus, use=use, visual=visual, width=width)
-        self.title = title
+    manager: Manager
+    text_area: ScrolledText
+    input_field: tk.Entry
+    key: selectors.SelectorKey
+    messages: str
+    room_id: int
+    is_failed: bool
 
-sel = selectors.DefaultSelector()
-messages = b""
+    def __init__(self, manager: Manager, room_id: int = -1, sk: selectors.SelectorKey = None, master=None):
+        super().__init__(master=master)  # initialize the window object
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            display_message(f"Received {recv_data.decode()}")
-            data.recv_total += len(recv_data)
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
+        self.manager = manager
 
-def display_message(message: str):
-    global text_area
-    text_area.config(state=tk.NORMAL)
-    text_area.insert(tk.END, message + "\n")
-    text_area.yview(tk.END)
-    text_area.config(state=tk.DISABLED)
+        self.key = sk
+        self.room_id = room_id
 
-def send_message(event=None):
-    global messages
-    if input_field.get():
-        if input_field.get().startswith("/"):
-            cmd(input_field.get()[1:], sk, root, display_message)
-        else:
-            display_message("Sent: " + input_field.get())
-            messages = f"{sk.data.name} said: {input_field.get()}"
-            messages = messages.encode()
-            sk.data.outb += messages
-            messages = b""
-        input_field.delete(0, tk.END)
+        self.title(f"Chat Client -- welcome for using, {sk.data.name} -- room id: {self.room_id}")
+        self.text_area = ScrolledText(self, wrap=tk.WORD, state=tk.DISABLED)
+        self.text_area.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+        self.text_area.config(state=tk.NORMAL)
+
+        self.input_field = tk.Entry(self)
+        self.input_field.pack(padx=10, pady=10, fill=tk.X)
+        self.input_field.bind("<Return>", self.send_message)
+
+        self.is_failed = False
+
+        self.display_message(
+            "Welcome to use EASY COMM, developed by Jingtian Hu at the University of Toronto. For help, enter \"/help\"")
+
+    def service_connection(self, key, mask):
+        sock: socket.socket = key.fileobj
+        data = key.data
+        if mask & selectors.EVENT_READ:
+            recv_data: bytes = sock.recv(1024)  # Should be ready to read
+            if recv_data:
+                if recv_data.startswith(STX):
+                    self.display_message(f"Received from server: {recv_data.decode()}")
+                    if b"fail" in recv_data:
+                        self.input_field.config(state=tk.DISABLED)
+                else:
+                    self.display_message(f"Received {recv_data.decode()}")
+                    data.recv_total += len(recv_data)
+            else:
+                if not self.is_failed:
+                    self.display_message(f"Server closed the connection, please close this window and try again")
+                    self.is_failed = True
+
+        if mask & selectors.EVENT_WRITE:
+            if data.outb:
+                sent = sock.send(data.outb)  # Should be ready to write
+                data.outb = data.outb[sent:]
+
+    def display_message(self, message: str):
+        self.text_area.config(state=tk.NORMAL)
+        self.text_area.insert(tk.END, message + "\n")
+        self.text_area.yview(tk.END)
+        self.text_area.config(state=tk.DISABLED)
+
+
+    def send_message(self, event=None):
+        if self.input_field.get():
+            if self.input_field.get().startswith("/"):
+                cmd(self.input_field.get()[1:], self.key, self, self.display_message)
+            else:
+                self.display_message("Sent: " + self.input_field.get())
+                self.messages = f"{self.key.data.name} said: {self.input_field.get()}"
+                self.key.data.outb += self.messages.encode()
+                self.messages = ""
+            self.input_field.delete(0, tk.END)
+
+    def on_closing(self):
+        self.manager.close_chat_window(self)
 
 
 
-    
+class Manager():
+    sel: selectors.DefaultSelector
+    chat_windows: set[ChatWindow]
+    root: tk.Tk
+    name: str
+    server_addr: tuple[str, int]
 
-host, port, name = '74.48.98.218', sys.argv[1], sys.argv[2]
+    def __init__(self, host, port, name):
+        self.server_addr = (host, int(port))
+        self.name = name
+        self.sel = selectors.DefaultSelector()
+        # self.sel.register(socket.socket(), 1) # add a useless socket to prevent sel.select() error
+        self.chat_windows = set()
+        self.root = tk.Tk()
+
+        self.root.title(f"Easy_comm - Welcome back, {name}!")
+        self.root.geometry("400x300")  # width x height
+
+        # Create input fields
+        self.room_label = ttk.Label(self.root, text="Room ID:")
+        self.room_label.pack()
+        self.room_entry = ttk.Entry(self.root)
+        self.room_entry.pack()
+
+        self.pass_label = ttk.Label(self.root, text="Password:")
+        self.pass_label.pack()
+        self.pass_entry = ttk.Entry(self.root)
+        self.pass_entry.pack()
+
+        # Create buttons
+        self.join_button = ttk.Button(self.root, text="join", command=lambda: self.add_new_conn("join"))
+        self.join_button.pack(pady=5)
+
+        self.create_button = ttk.Button(self.root, text="create", command=lambda: self.add_new_conn("create"))
+        self.create_button.pack(pady=5)
+        # self.room_entry.insert(0, "1")
+        # self.add_new_conn("join")
+
+    def start(self):
+        self.root.after(20, self.check_socket)
+        self.root.mainloop()
+
+    def add_new_conn(self, operation: str):
+        """
+        creates a chatwindow with a newly created socket, then try to validate with the server with a formatted message.
+        """
+        room_id = int(self.room_entry.get())
+        self.room_entry.delete(0, tk.END)
+        password = int(self.pass_entry.get())
+        self.pass_entry.delete(0, tk.END)
+        print(f"Starting connection to {server_addr} room number {room_id}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            recv_total=0,
+            outb=b"",
+            name=self.name,
+            window=-1,
+        )
+        key = self.sel.register(sock, events, data=data)
+        chat_window = ChatWindow(manager=self, room_id=room_id, sk=key)
+        data.window = chat_window
+
+        if operation == "join":
+            key.data.outb += f"join,{room_id},{password},".encode() + ETX
+        if operation == "create":
+            key.data.outb += f"create,{room_id},{password},".encode() + ETX
+
+        self.chat_windows.add(chat_window)
+
+    def close_chat_window(self, chat_window: ChatWindow):
+        print("before closing:")
+        print(self.chat_windows)
+        print(dict(self.sel.get_map()))
+        self.chat_windows.remove(chat_window)
+        self.sel.unregister(chat_window.key.fileobj)
+        chat_window.key.fileobj.close()
+        chat_window.destroy()
+        print("after closing:")
+        print(self.chat_windows)
+        print(dict(self.sel.get_map()))
+
+    def quit(self):
+        self.root.quit()
+        curr_conns = dict(self.sel.get_map())
+        for fd in curr_conns:
+            key = curr_conns[fd]
+            key.fileobj.close()
+        self.sel.close()
+
+    def check_socket(self):
+        # print("check+socket working properly")
+        if len(self.chat_windows) > 0:
+            events = self.sel.select(timeout=0)
+            if events:
+                for key, mask in events:
+                    key.data.window.service_connection(key, mask)
+        self.root.after(20, self.check_socket)
+
+
+host, port, name = '127.0.0.1', sys.argv[1], sys.argv[2]
 print(host, port, name)
 
 server_addr = (host, int(port))
-print(f"Starting connection to {server_addr}")
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setblocking(False)
-sock.connect_ex(server_addr)
-events = selectors.EVENT_READ | selectors.EVENT_WRITE
-data = types.SimpleNamespace(
-    recv_total=0,
-    outb=b"",
-    name=name
-)
-sk = sel.register(sock, events, data=data)
 
-root = tk.Tk()
-root.title(f"Chat Client -- welcome for using, {sk.data.name}")
-
-
-
-text_area = ScrolledText(root, wrap=tk.WORD, state=tk.DISABLED)
-text_area.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
-
-input_field = tk.Entry(root)
-input_field.pack(padx=10, pady=10, fill=tk.X)
-input_field.bind("<Return>", send_message)
-
-display_message("Welcome to use LETS CHAT, developed by Jingtian Hu at the University of Toronto. For help, enter \"/help\"")
-
-def check_socket():
-    events = sel.select(timeout=None)
-    if events:
-        for key, mask in events:
-            service_connection(key, mask)
-    root.after(20, check_socket)
-
+manager = Manager(host=host, port=port, name=name)
 
 try:
-    root.after(20, check_socket)
-    root.mainloop()
+    manager.start()
 except KeyboardInterrupt:
     print("ctrl+c detected exiting")
 finally:
-    sock.close()
-    sel.close()
-    root.quit()
-
-
+    manager.quit()
