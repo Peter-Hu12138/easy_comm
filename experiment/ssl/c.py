a507ff416f3bb936d8218107961ef558721c236b
Jingtian Hu <jt.hu@mail.utoronto.ca>
2025-11-11 17:51:01 -0500

    added testing certificates

--- experiment/ssl/c.py
+++ experiment/ssl/c.py
@@ -4,6 +4,7 @@ import ssl
 hostname = '127.0.0.1'
 context = ssl.create_default_context()
 
+
 with socket.create_connection((hostname, 8000)) as sock:
     with context.wrap_socket(sock, server_hostname=hostname) as ssock:
         print(ssock.version())
